079ce76fa74c802c2a436e10f9878edd9750623f
AntonHansen <anton.hansen@hotmail.com>
2024-05-24 17:24:16 +0200

    24-may-commit-2

--- src/pages/BookInfo/BookInfo.tsx
+++ src/pages/BookInfo/BookInfo.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 
 interface Excerpts {
 	excerpt: string;
@@ -21,66 +22,66 @@ const BookInfo = () => {
 	const [book, setBook] = useState<Book | null>(null);
 	const [firstTenSubjects, setFirstTenSubjects] = useState<string>('');
 
+	const { data, error } = useFetch(`${URL}${id}.json`);
+	// const jsonString = JSON.stringify(data);
+	// console.log(jsonString);
+	console.log('Data console log: ', data);
+	console.log('Error console log: ', error);
+
 	useEffect(() => {
-		async function getBookInfo() {
-			try {
-				const response = await fetch(`${URL}${id}.json`);
-				const data = await response.json();
+		if (data) {
+			const { description, title, covers, subjects, excerpts } = data;
+			let bookDescription = 'No description found';
 
-				if (data) {
-					const { description, title, covers, subjects, excerpts } = data;
-					let bookDescription = 'No description found';
-
-					if (description) {
-						if (typeof description === 'string') {
-							bookDescription = description;
-						} else if (description.value) {
-							bookDescription = description.value;
-						}
-					}
-
-					const newBook: Book = {
-						description: bookDescription,
-						title: title,
-						cover_img: covers
-							? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`
-							: 'No cover found',
-						subjects: subjects || [],
-						excerpts: excerpts || [],
-					};
-					setBook(newBook);
-
-					const firstTen = (subjects || []).slice(0, 10);
-					setFirstTenSubjects(JSON.stringify(firstTen));
-				} else {
-					setBook(null);
+			if (description) {
+				if (typeof description === 'string') {
+					bookDescription = description;
+				} else if (description.value) {
+					bookDescription = description.value;
 				}
-			} catch (error) {
-				console.log(error);
 			}
+
+			const newBook: Book = {
+				description: bookDescription,
+				title: title,
+				cover_img: covers
+					? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`
+					: 'No cover found',
+				subjects: subjects || [],
+				excerpts: excerpts || [],
+			};
+			setBook(newBook);
+
+			const firstTen = (subjects || []).slice(0, 10);
+			setFirstTenSubjects(JSON.stringify(firstTen));
+		} else if (error) {
+			console.log(error);
 		}
-		getBookInfo();
 	}, [id]);
 
 	return (
 		<section className="py-24">
 			<div className="container mx-auto">
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-					<div className="max-h-[600px] overflow-hidden">
-						<img src={book?.cover_img} alt="CoverArt" className="w-full" />
+					<div className="overflow-hidden flex justify-center items-center">
+						<img
+							src={book?.cover_img}
+							alt="CoverArt"
+							className="w-full md:w-auto"
+						/>
 					</div>
-					<div className="overflow-y-auto max-h-[600px] px-4">
+					<div className="overflow-y-auto px-4">
 						<div className="mb-6">
 							<span className="font-semibold text-3xl">{book?.title}</span>
 						</div>
-						<div className="opacity-80">
+						<div>
 							<span>{book?.description}</span>
 						</div>
-						<div className="opacity-80">
+						<div>
 							<h4 className="font-semibold">Subjects:</h4>
 							<span>{firstTenSubjects}</span>
 						</div>
-						<div className="opacity-80">
+						<div>
 							{book?.excerpts &&
 								book.excerpts.map((excerpt, index) => (
 									<div key={index} className="mb-4">
